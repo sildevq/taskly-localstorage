c06117dcfee6eb975433a6252406e230e574411d
sil <aziatkg104@gmail.com>
2025-09-30 00:41:28 +0300

    fix lang

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,27 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./locales/en.json";
 import ru from "./locales/ru.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    ru: { translation: ru },
-    en: { translation: en },
-  },
-  lng: "ru", // язык по умолчанию
-  fallbackLng: "ru",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(LanguageDetector) // подключаем детектор
+  .use(initReactI18next)
+  .init({
+    resources: {
+      ru: { translation: ru },
+      en: { translation: en },
+    },
+    fallbackLng: "ru",
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["localStorage", "navigator"], // проверяем localStorage и язык браузера
+      caches: ["localStorage"], // сохраняем выбор в localStorage
+    },
+  });
 
 export default i18n;
